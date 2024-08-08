240f7d7df4cf67030c97d5005371a548bd47ecf0
Josef Leventon <josef.leventon@gmail.com>
2024-08-08 16:29:55 -0400

    Remove sw fetch

--- public/service-worker.js
+++ public/service-worker.js
@@ -6,14 +6,6 @@ self.addEventListener('activate', (event) => {
   console.log('Service Worker activated')
 })
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request)
-    })
-  )
-})
-
 self.addEventListener('push', (event) => {
   const data = event.data.json()
   const options = {
